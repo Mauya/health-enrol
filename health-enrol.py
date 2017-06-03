1c418765a05c635a3b2e4817095d496a5abe257c
Mauya <smkadungure@live.co.uk>
2017-06-03 15:34:12 +0100

    project first commit

---
+++ health-enrol.py
@@ -0,0 +1,10 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+@app.route('/')
+def home():
+   return render_template('home.html')
+@app.route('/dash')
+def dash():
+   return render_template('dash.html')
+if __name__ == '__main__':
+   app.run(debug=False)
