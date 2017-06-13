ce0be88831016158fcd5c39a5b9e3c5ad620abdc
Mauya <smkadungure@live.co.uk>
2017-06-14 00:09:48 +0100

    selectors graphed

--- static/js/graph.js
+++ static/js/graph.js
@@ -16,7 +16,7 @@ function makeGraphs(error, projectsJson) {
     // Dimensions
     var dateDim = ndx.dimension(function (d) {
         return d["Academic_Year"];
-     });
+    });
     var CourseNameDim = ndx.dimension(function (d) {
         return d["Course"];
     });
@@ -32,57 +32,59 @@ function makeGraphs(error, projectsJson) {
     // var ReasonForLeavingDim = ndx.dimension(function (d) {
     //     return d["Reason_For_Leaving"];
     // });
-    var totalEnrollmentDim = ndx.dimension(function (d) {
-        return d["starters"];
-    // });
-    // var FeesDim = ndx.dimension(function (d) {
-    //     return d["Fees"];
-    // });
-    // var GenderDim = ndx.dimension(function (d) {
-    //     return d["Gender"];
-    // });
-    // var AgeDim = ndx.dimension(function (d) {
-    //     return d["Age"];
-    // });
-    // var WhiteBMEDim = ndx.dimension(function (d) {
-    //     return d["White_BME"];
-    // });
-    // var EthnicityGroupDim = ndx.dimension(function (d) {
-    //     return d["Ethnicity_Group"];
-    // });
-    // var DisabilityDescriptionDim = ndx.dimension(function (d) {
-    //     return d["Disability_Description"];
-    // });
-    // var DisabilityDim = ndx.dimension(function (d) {
-    //     return d["Disability_YN"];
-    // });
-    // var WithdrawalsDim = ndx.dimension(function (d) {
-    //     return d["Withdrawals"];
-    // });
+    // var totalEnrollmentDim = ndx.dimension(function (d) {
+    //     return d["starters"];
+        // });
+        // var FeesDim = ndx.dimension(function (d) {
+        //     return d["Fees"];
+        // });
+        // var GenderDim = ndx.dimension(function (d) {
+        //     return d["Gender"];
+        // });
+        // var AgeDim = ndx.dimension(function (d) {
+        //     return d["Age"];
+        // });
+        // var WhiteBMEDim = ndx.dimension(function (d) {
+        //     return d["White_BME"];
+        // });
+        // var EthnicityGroupDim = ndx.dimension(function (d) {
+        //     return d["Ethnicity_Group"];
+        // });
+        // var DisabilityDescriptionDim = ndx.dimension(function (d) {
+        //     return d["Disability_Description"];
+        // });
+        // var DisabilityDim = ndx.dimension(function (d) {
+        //     return d["Disability_YN"];
+        // });
+        // var WithdrawalsDim = ndx.dimension(function (d) {
+        //     return d["Withdrawals"];
+        // });
 
 
 // Groups- calculate metrics
-    var numProjectsByDate = dateDim.group();
-    var numProjectsByCourseName = CourseNameDim.group();
-    // var numProjectsByCourseStage = CourseStageDim.group();
-    // var numProjectsByModeGroupDim = ModeGroupDim.group();
-    // var numProjectsByLevelGroupDim = LevelGroupDim.group();
-    // var numProjectsByReasonForLeaving = ReasonForLeavingDim.group();
-    var totalEnrollment = ndx.groupAll().reduceSum(function(d) {return d["starters"];});
-    // var numProjectsByGender = GenderDim.group();
-    // var numProjectsByAge = AgeDim.group();
-    // var numProjectsByFees = FeesDim();
-    // var numProjectsByWhiteBME = WhiteBMEDim.group();
-    // var numProjectsByEthnicityGroup = EthnicityGroupDim.group();
-    // var numProjectsByDisabilityDescription = DisabilityDescriptionDim.group();
-    // var numProjectsByDisability = DisabilityDim.group();
-    // var numProjectsByWithdrawals = WithdrawalsDim.group();
+        var numProjectsByDate = dateDim.group();
+        var numProjectsByCourseName = CourseNameDim.group();
+        // var numProjectsByCourseStage = CourseStageDim.group();
+        // var numProjectsByModeGroupDim = ModeGroupDim.group();
+        // var numProjectsByLevelGroupDim = LevelGroupDim.group();
+        // var numProjectsByReasonForLeaving = ReasonForLeavingDim.group();
+        // var totalEnrollment = ndx.groupAll().reduceSum(function (d) {
+        //     return d["starters"];
+        // });
+        // var numProjectsByGender = GenderDim.group();
+        // var numProjectsByAge = AgeDim.group();
+        // var numProjectsByFees = FeesDim();
+        // var numProjectsByWhiteBME = WhiteBMEDim.group();
+        // var numProjectsByEthnicityGroup = EthnicityGroupDim.group();
+        // var numProjectsByDisabilityDescription = DisabilityDescriptionDim.group();
+        // var numProjectsByDisability = DisabilityDim.group();
+        // var numProjectsByWithdrawals = WithdrawalsDim.group();
 
 //all
 //     var all = ndx.groupAll();
-    var totalEnrollment = ndx.groupAll().reduceSum(function (d) {
-        return d["starters"];
-    })
+//         var totalEnrollment = ndx.groupAll().reduceSum(function (d) {
+//             return d["starters"];
+//         });
 //     var max_course = totalEnrollmentByCourse.top(1)[0].value;
 //     var minDate = dateDim.bottom(1)[0]["Academic_Year"];
 //     var maxDate = dateDim.top(1)[0]["Academic_Year"];
@@ -128,7 +130,7 @@ function makeGraphs(error, projectsJson) {
         // Displayed in dash.html
         var selectFieldYear = dc.selectMenu('#menu-select-academic_year');
         var selectFieldCourse = dc.selectMenu('#menu-select-course');
-        var totalEnrollmentND = dc.numberDisplay('#total-enrollment-nd')
+        // var totalEnrollmentND = dc.numberDisplay('#total-enrollment-nd');
         // var totalInternationalND = dc.numberDisplay("#total-international-nd");
         // var totalHomeEuND = dc.numberDisplay("#total-home/eu-nd");
         // var totalWithdrawalsND = dc.numberDisplay("#total-withdrawals-nd");
@@ -147,12 +149,12 @@ function makeGraphs(error, projectsJson) {
             .dimension(CourseNameDim)
             .group(numProjectsByCourseName);
 
-        totalEnrollmentND
-            .formatNumber(totalEnrollment)
-            .valueAccessor(function (d) {
-                return d;
-            })
-            .group(totalEnrollment);
+        // totalEnrollmentND
+        //     .formatNumber(totalEnrollment)
+        //     .valueAccessor(function (d) {
+        //         return d;
+        //     })
+        //     .group(totalEnrollment);
 
         // totalWithdrawalsND
         //     .formatNumber(d3.format("d"))
@@ -163,5 +165,7 @@ function makeGraphs(error, projectsJson) {
 
 
         dc.renderAll();
-    })
+
+    });
+}
 
